fcc4ca9c74b5c2254f3805595e70a55a76169296
aneesazc <anees.azc@gmail.com>
2024-04-21 23:46:45 +0530

    add workflow

--- apps/user-app/components/SendMoneyCard.tsx
+++ apps/user-app/components/SendMoneyCard.tsx
@@ -1,11 +1,9 @@
 "use client"
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
-import { Center } from "@repo/ui/center";
-import { Select } from "@repo/ui/select";
 import { useState } from "react";
 import { TextInput } from "@repo/ui/textinput";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { peerToPeerTransactions } from "../app/lib/actions/peerToPeerTransactions";
 
 const SUPPORTED_BANKS = [{
