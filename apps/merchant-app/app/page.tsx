cc3ce397036ca391cd380924344315e611cc0433
aneesazc <anees.azc@gmail.com>
2024-04-22 00:21:14 +0530

    testing

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -3,7 +3,7 @@
 //@ts-ignore
 import { useBalance } from "@repo/store/balance";
 
-export default function() {
+export default function () {
   const balance = useBalance();
   return <div>
     hi there {balance}
